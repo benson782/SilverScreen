7d2746ae97f81c1f2507b8ae4a0dba22585c2eac
Benson Malonzo <benson782@hotmail.com>
2015-09-19 21:54:41 -0700

    add FetchMovieTask class to get the movie data from The Movie DB add permission to use Internet add method updateMovies to fetch the movie data

--- app/src/main/java/com/example/android/silverscreen/MainActivityFragment.java
+++ app/src/main/java/com/example/android/silverscreen/MainActivityFragment.java
@@ -1,13 +1,21 @@
 package com.example.android.silverscreen;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -31,7 +39,6 @@ public class MainActivityFragment extends Fragment {
                 "Movie 1", "Movie 2", "Movie 3", "Movie 4", "Movie 5",
                 "Movie 6", "Movie 7", "Movie 8", "Movie 9", "Movie 10",
                 "Movie 11", "Movie 12", "Movie 13", "Movie 14", "Movie 15",
-                "Movie 16", "Movie 17", "Movie 18", "Movie 19", "Movie 20",
 
         };
         List<String> dummyData = new ArrayList<>(Arrays.asList(data));
@@ -60,7 +67,90 @@ public class MainActivityFragment extends Fragment {
             }
         });
 */
+        updateMovies();
 
         return rootView;
+    }
+
+    private void updateMovies() {
+        FetchMovieTask movieTask = new FetchMovieTask();
+        movieTask.execute();
+    }
+
+    public class FetchMovieTask extends AsyncTask<Void, Void, Void> {
+
+        private final String LOG_TAG = FetchMovieTask.class.getSimpleName();
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String movieJsonStr = null;
+
+            try {
+                // Construct the URL for the The Movies DB query
+                // Possible parameters are available at TMDb's API page, at
+                // http://openweathermap.org/API#forecast
+                URL url = new URL(
+                        "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" +
+                        getString(R.string.my_api_key));
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    //forecastJsonStr = null;
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    //forecastJsonStr = null;
+                    return null;
+                }
+                movieJsonStr = buffer.toString();
+
+                Log.v(LOG_TAG,"Movie data: " + movieJsonStr);
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the movie data, there's no point in attempting
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            return null;
+        }
+
     }
 }
