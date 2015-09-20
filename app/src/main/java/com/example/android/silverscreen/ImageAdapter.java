0182a9a75b74990ed0daf5c6506477134c6ff885
Benson Malonzo <benson782@hotmail.com>
2015-09-20 11:36:31 -0700

    removed padding on fragment. made it go to edges. trying to make the movie images match UC mockup

--- app/src/main/java/com/example/android/silverscreen/ImageAdapter.java
+++ app/src/main/java/com/example/android/silverscreen/ImageAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -11,13 +12,12 @@ import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 
 
-public class ImageAdapter extends ArrayAdapter<String>{
+public class ImageAdapter extends ArrayAdapter<String> {
 
     private final String LOG_TAG = ImageAdapter.class.getSimpleName();
 
     public ImageAdapter(Context context, ArrayList<String> str) {
         super(context, 0, str);
-
     }
 
     @Override
@@ -26,13 +26,14 @@ public class ImageAdapter extends ArrayAdapter<String>{
         if (convertView == null) {
             // if it's not recycled, initialize some attributes
             imageView = new ImageView(getContext());
+            imageView.setLayoutParams(new GridView.LayoutParams(185 * 2, 277 * 2));
         } else {
             imageView = (ImageView) convertView;
         }
 
-        // Use Picasso to set the image
+        // Use Picasso to fetch the image and set in the ImageView
         String url = "http://image.tmdb.org/t/p/w185" + getItem(position);
-        Picasso.with(getContext()).load(url).into(imageView);
+        Picasso.with(getContext()).load(url).fit().centerCrop().into(imageView);
 
         return imageView;
     }
